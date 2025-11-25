73b6d27720f34c3a69d58f5450f320174bd80577
Alex Sobolev <aleksandr.sobolev@reef.pl>
2025-11-26 02:07:26 +0600

    Minor adjustments based on the PR feedback

--- app/src/project/core/block_tasks.py
+++ app/src/project/core/block_tasks.py
@@ -1,7 +1,7 @@
 from abstract_block_dumper.v1.decorators import block_task
 
 
-@block_task(condition=lambda bn: True, celery_kwargs={"queue": "celery"})  # noqa: ARG005
-def process_every_block(block_number: int) -> int:
-    print(f"Processing every block: {block_number}")
+@block_task(condition=lambda block_number: block_number % 2 == 0)
+def process_every_even_block(block_number: int) -> int:
+    print(f"Processing every even block: {block_number}")
     return block_number
