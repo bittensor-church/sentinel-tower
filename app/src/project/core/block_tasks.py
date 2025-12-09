169ee67718bbeaa89f74df147b71753b1bf064c5
Alex Sobolev <aleksandr.sobolev@reef.pl>
2025-12-09 13:24:07 +0600

    Unify block ingestions

--- app/src/project/core/block_tasks.py
+++ app/src/project/core/block_tasks.py
@@ -10,14 +10,24 @@ logger = structlog.get_logger()
 
 
 @block_task(celery_kwargs={"rate_limit": "10/m"})
-def store_hyperparameters(block_number: int) -> str:
+def store_blockchain_data(block_number: int) -> str:
+    """
+    Store extrinsics from the given block number that contain hyperparameter updates.
+    """
+    service = sentinel_service(bittensor_provider())
+    block = service.ingest_block(block_number)
+
+    hyperparam_extrinsics = store_hyperparam_extrinsics(block, block_number)
+    set_weights_extrinsics = store_set_weights_extrinsics(block, block_number)
+
+    return f"{hyperparam_extrinsics}\n{set_weights_extrinsics}".strip()
+
+
+def store_hyperparam_extrinsics(block, block_number: int) -> str:
     """
     Store extrinsics from the given block number that contain hyperparameter updates.
     """
     hyperparams_storage = JsonLinesStorage("data/bittensor/hyperparams-extrinsics.jsonl")
-
-    service = sentinel_service(bittensor_provider())
-    block = service.ingest_block(block_number)
     hyperparam_extrinsics = extrinsics_filters.filter_hyperparam_extrinsics(block.extrinsics)
     if not hyperparam_extrinsics:
         return ""
@@ -30,20 +40,14 @@ def store_hyperparameters(block_number: int) -> str:
 
     for extrinsic in hyperparam_extrinsics:
         hyperparams_storage.append({"block_number": block_number, **extrinsic.model_dump()})
-
     return f"Stored {len(hyperparam_extrinsics)} hyperparameter extrinsics from block {block_number}"
 
 
-@block_task(celery_kwargs={"rate_limit": "10/m"})
-def store_set_weights_extrinsics(block_number: int) -> str:
+def store_set_weights_extrinsics(block, block_number: int) -> str:
     """
-    Store extrinsics from the given block number that contain set weight updates.
+    Store extrinsics from the given block number that contain set_weights calls.
     """
     weights_storage = JsonLinesStorage("data/bittensor/set-weights-extrinsics.jsonl")
-
-    service = sentinel_service(bittensor_provider())
-    block = service.ingest_block(block_number)
-
     set_weights_extrinsics = extrinsics_filters.filter_weight_set_extrinsics(block.extrinsics)
     if not set_weights_extrinsics:
         return ""
@@ -56,5 +60,4 @@ def store_set_weights_extrinsics(block_number: int) -> str:
 
     for extrinsic in set_weights_extrinsics:
         weights_storage.append({"block_number": block_number, **extrinsic.model_dump()})
-
-    return f"Stored {len(set_weights_extrinsics)} set weights extrinsics from block {block_number}"
+    return f"Stored {len(set_weights_extrinsics)} set_weights extrinsics from block {block_number}"
