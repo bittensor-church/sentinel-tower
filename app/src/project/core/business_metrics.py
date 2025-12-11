4399c62023d6f14af2a9295ef3aa30c592e54671
Alex Sobolev <aleksandr.sobolev@reef.pl>
2025-12-11 22:02:29 +0600

    BACT-220 Added grafana

--- app/src/project/core/business_metrics.py
+++ app/src/project/core/business_metrics.py
@@ -1,5 +1,13 @@
 from django_business_metrics.v0 import BusinessMetricsManager, active_users, users
 
+from project.core.models import HyperparamEvent
+
+
+def hyperparam_events_total():
+    """Return total count of hyperparameter events."""
+    return HyperparamEvent.objects.count()
+
+
 metrics_manager = BusinessMetricsManager()
 
-metrics_manager.add(users).add(active_users)
+metrics_manager.add(users).add(active_users).add(hyperparam_events_total)
