fcca07aa821fdc14dbc51888a3a8ec17c57b4682
Aleksandr Sobolev <aleksandr.sobolev@reef.pl>
2026-06-08 16:49:52 +0600

    fix(celery-worker): update entrypoin to properly mount logs volume

--- app/envs/prod/celery-entrypoint.sh
+++ app/envs/prod/celery-entrypoint.sh
@@ -7,13 +7,17 @@ set -eu
 # each worker may have its own settings
 WORKERS="master worker metagraph"
 MAX_TASKS_PER_CHILD="${CELERY_WORKER_MAX_TASKS_PER_CHILD:-50}"
-OPTIONS="-A project -E -l ERROR --pidfile=/var/run/celery-%n.pid --logfile=/var/log/celery-%n.log --max-tasks-per-child=$MAX_TASKS_PER_CHILD"
+
+# The container runs as the unprivileged "appuser", which cannot write to
+# /var/log; worker logs go to /tmp/logs (pidfiles use the writable /run tmpfs).
+mkdir -p /tmp/logs
+OPTIONS="-A project -E -l ERROR --pidfile=/var/run/celery-%n.pid --logfile=/tmp/logs/celery-%n.log --max-tasks-per-child=$MAX_TASKS_PER_CHILD"
 
 # set up settings for workers and run the latter;
 # here events from "celery" queue (default one, will be used if queue not specified)
 # will go to "master" workers, and events from "worker" queue go to "worker" workers;
 # by default there are no workers, but each type of worker may scale up to 4 processes
-# Since celery runs in root of the docker, we also need to allow it to.
+# C_FORCE_ROOT is a harmless no-op under appuser, kept in case this is ever run as root.
 # shellcheck disable=2086
 C_FORCE_ROOT=1 nice celery multi start $WORKERS $OPTIONS \
     -Q:master celery --autoscale:master=$CELERY_MASTER_CONCURRENCY,0 \
@@ -23,7 +27,7 @@ C_FORCE_ROOT=1 nice celery multi start $WORKERS $OPTIONS \
 # shellcheck disable=2064
 trap "celery multi stop $WORKERS $OPTIONS; exit 0" INT TERM
 
-tail -f /var/log/celery-*.log &
+tail -f /tmp/logs/celery-*.log &
 
 # check celery status periodically to exit if it crashed
 while true; do
